690163ebad181294305e8d0ce5e62ab9574a3713
kspugachev <kspugachev@mail.ru>
2024-07-16 13:13:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(1234 % 1000 // 10 + 5678 % 1000 // 10)
+
+# 4th program
+print(int(13.42) == int(42.13 * 100 % 4200) or int(42.13) == int(13.42 * 100 % 1300))
