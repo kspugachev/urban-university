0963d435baeb35fb64196e656898628a01c53d54
Konstantin Pugachev <kspugachev@mail.ru>
2024-07-16 22:01:28 +0300

    Update homework2.py
    Внесены изменения, так как в предыдущем варианте код не в полной мере соответствовал требованиям задания. Добавлен альтернативный вариант кода в виде комментария.

--- homework2.py
+++ homework2.py
@@ -8,6 +8,10 @@ homework_time = 1.5
 course_name = 'Python'
 one_task_time = homework_time / homework_amount
 
-print('Курс: ' + course_name + ', всего задач:' + str(homework_amount) + ', затрачено часов: ' + str(homework_time) +
-      ', среднее время выполнения: ' + str(one_task_time) + ' часа.')
-# Между "всего задач:" и "12" пробел намеренно пропущен, т.к. в задании указано именно так.
+# В задании указано, что пробелы между словами и символами ":" "," можно ставить на свое усмотрение.
+print('Курс:', course_name, ', всего задач:', str(homework_amount), ', затрачено часов:', str(homework_time),
+      ', среднее время выполнения:', str(one_task_time), 'часа.')
+
+# Используем конкатенацию строк, чтобы вывод текста был идентичен тексту в задании.
+# print('Курс: ' + course_name + ', всего задач:' + str(homework_amount) + ', затрачено часов: ' + str(homework_time) +
+#       ', среднее время выполнения: ' + str(one_task_time) + ' часа.')
