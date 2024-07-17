aeef4583e5f7910f3b2452900239f3f2b126ec85
Konstantin Pugachev <kspugachev@mail.ru>
2024-07-17 15:22:24 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,16 @@
+"""
+Практическая работа по уроку "Динамическая типизация".
+Цель: написать программу на языке Python, используя PyCharm, для демонстрации динамической типизации.
+"""
+
+name = 'Konstantin'
+print(name)
+
+age = 38
+print(age)
+
+age = age + 1
+print(age)
+
+is_student = True
+print(is_student)
