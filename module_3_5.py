414dd4a6a5d4f5666e774f57802a4e40dc710913
kspugachev <kspugachev@mail.ru>
2024-08-05 18:11:57 +0300

    Ver_1

---
+++ module_3_5.py
@@ -0,0 +1,17 @@
+"""
+Самостоятельная работа по уроку "Рекурсия".
+Цель: написать функцию, которая принимает целое число и подсчитывает произведение цифр этого числа.
+"""
+
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+
+result = get_multiplied_digits(40203)
+print(result)
