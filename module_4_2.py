3cea723a64663e44ba2520dd4c1c28ddba0cb885
kspugachev <kspugachev@mail.ru>
2024-08-16 12:53:06 +0300

    Ver_1

---
+++ module_4_2.py
@@ -0,0 +1,12 @@
+"""
+Домашняя работа по уроку "Пространство имен".
+"""
+
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
+
+
+test_function()
+#inner_function()  # We can't call a function from the local space of another function.
